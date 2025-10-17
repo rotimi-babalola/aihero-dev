66e10bd5c8b18f226a72a31f01c6ae931b5b53f0
Rotimi Babalola <tolurotimi@hotmail.com>
2025-10-17 17:29:49 +0200

    Implement message handling in chat API and client

--- exercises/01-ai-sdk-basics/01.07-stream-text-to-ui/problem/api/chat.ts
+++ exercises/01-ai-sdk-basics/01.07-stream-text-to-ui/problem/api/chat.ts
@@ -4,24 +4,27 @@ import {
   streamText,
   type ModelMessage,
   type UIMessage,
+  convertToModelMessages,
 } from 'ai';
 
 export const POST = async (req: Request): Promise<Response> => {
   const body = await req.json();
 
   // TODO: get the UIMessage[] from the body
-  const messages: UIMessage[] = TODO;
+  const messages: UIMessage[] = body.messages;
 
   // TODO: convert the UIMessage[] to ModelMessage[]
-  const modelMessages: ModelMessage[] = TODO;
+  const modelMessages: ModelMessage[] =
+    convertToModelMessages(messages);
 
   // TODO: pass the modelMessages to streamText
   const streamTextResult = streamText({
     model: google('gemini-2.0-flash'),
+    messages: modelMessages,
   });
 
   // TODO: create a UIMessageStream from the streamTextResult
-  const stream = TODO;
+  const stream = streamTextResult.toUIMessageStream();
 
   return createUIMessageStreamResponse({
     stream,
